66c875e5297b9ef0a13731c362978ee24968259e
Keith <youngka@gmail.com>
2024-01-23 10:04:29 -0500

    initial infra creation script

---
+++ cloud-shell-infra-init.sh
@@ -0,0 +1,5 @@
+gcloud compute networks create php-01hhmj81fhp-vpc-01 --project=php-01hhmj81fhp --description=Network\ for\ ACM\ Config\ Controller\ for\ DNS --subnet-mode=custom --mtu=1460 --bgp-routing-mode=regional 
+
+gcloud compute networks subnets create php-01hhmj81fhp-vpc-01-sub-01 --project=php-01hhmj81fhp --range=10.0.0.0/24 --stack-type=IPV4_ONLY --network=php-01hhmj81fhp-vpc-01 --region=northamerica-northeast1 --enable-private-ip-google-access
+
+gcloud beta container --project "php-01hhmj81fhp" clusters create-auto "php-01hhmj81fhp-dns-acm-config" --region "northamerica-northeast1" --release-channel "regular" --network "projects/php-01hhmj81fhp/global/networks/php-01hhmj81fhp-vpc-01" --subnetwork "projects/php-01hhmj81fhp/regions/northamerica-northeast1/subnetworks/php-01hhmj81fhp-vpc-01-sub-01" --cluster-ipv4-cidr "/17" --binauthz-evaluation-mode=DISABLED
