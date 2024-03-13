c07d1536377229740811837b7218d01107cbf6c9
John Bain <john.bain@gmail.com>
2024-03-13 11:46:20 -0400

    Update cloud-shell-infra-init.sh
    updated script so it can be reused for other instances

--- cloud-shell-infra-init.sh
+++ cloud-shell-infra-init.sh
@@ -1,31 +1,47 @@
-gcloud compute networks create php-01hhmj81fhp-vpc-01 \
-    --project=php-01hhmj81fhp \
-    --description=Network\ for\ ACM\ Config\ Controller\ for\ DNS \
+#!/bin/bash
+
+# Define the project ID and SA_EMAIL as variables
+PROJECT_ID="$1"
+
+if [ -z "$PROJECT_ID" ]; then
+    echo "Usage: $0 <project_id>"
+    echo "Example: $0 gcp-my-project "
+    exit 1
+fi
+
+
+# Create a GCP network
+gcloud compute networks create "${PROJECT_ID}-vpc-01" \
+    --project="${PROJECT_ID}" \
+    --description="Network for ACM Config Controller for DNS" \
     --subnet-mode=custom \
     --mtu=1460 \
-    --bgp-routing-mode=regional 
+    --bgp-routing-mode=regional
 
-gcloud compute networks subnets create php-01hhmj81fhp-vpc-01-sub-01 \
-    --project=php-01hhmj81fhp \
+# Create a subnet within the network
+gcloud compute networks subnets create "${PROJECT_ID}-vpc-01-sub-01" \
+    --project="${PROJECT_ID}" \
     --range=10.0.0.0/24 \
     --stack-type=IPV4_ONLY \
-    --network=php-01hhmj81fhp-vpc-01 \
+    --network="${PROJECT_ID}-vpc-01" \
     --region=northamerica-northeast1 \
     --enable-private-ip-google-access
 
-gcloud anthos config controller create php-01hhmj81fhp-dns \
+# Create an Anthos config controller
+gcloud anthos config controller create "${PROJECT_ID}-dns" \
     --location=northamerica-northeast1 \
     --full-management \
-    --network=projects/php-01hhmj81fhp/global/networks/php-01hhmj81fhp-vpc-01 \
-    --subnet=projects/php-01hhmj81fhp/regions/northamerica-northeast1/subnetworks/php-01hhmj81fhp-vpc-01-sub-01 
+    --network="projects/${PROJECT_ID}/global/networks/${PROJECT_ID}-vpc-01" \
+    --subnet="projects/${PROJECT_ID}/regions/northamerica-northeast1/subnetworks/${PROJECT_ID}-vpc-01-sub-01"
 
 # Configure config controller instance - https://cloud.google.com/anthos-config-management/docs/how-to/config-controller-setup#use-config-controller
-gcloud container clusters get-credentials krmapihost-php-01hhmj81fhp-dns --region northamerica-northeast1 --project php-01hhmj81fhp
+gcloud container clusters get-credentials "krmapihost-${PROJECT_ID}-dns" --region northamerica-northeast1 --project "${PROJECT_ID}"
 
-export SA_EMAIL="$(kubectl get ConfigConnectorContext -n config-control \
+SA_EMAIL="$(kubectl get ConfigConnectorContext -n config-control \
     -o jsonpath='{.items[0].spec.googleServiceAccount}' 2> /dev/null)"
 
-gcloud projects add-iam-policy-binding php-01hhmj81fhp \
+# Add IAM policy binding
+gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
     --member "serviceAccount:${SA_EMAIL}" \
     --role "roles/dns.admin" \
-    --project php-01hhmj81fhp
+    --project "${PROJECT_ID}"
