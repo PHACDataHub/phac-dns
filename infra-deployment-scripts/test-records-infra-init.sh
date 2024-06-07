9dd3db2563c9579c8a6189ceee0c07ede5a2d135
vedantthapa <thapavedant@gmail.com>
2024-06-07 13:17:10 -0300

    feat: provision asm

--- infra-deployment-scripts/test-records-infra-init.sh
+++ infra-deployment-scripts/test-records-infra-init.sh
@@ -15,6 +15,13 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
 
+# enable service mesh api
+gcloud services enable mesh.googleapis.com --project=${GOOGLE_CLOUD_PROJECT}
+
+# enable fleet mesh feature
+gcloud container fleet mesh enable --project=${GOOGLE_CLOUD_PROJECT}
+
+# setup a new cluster
 gcloud container clusters create-auto "records-test-phac-dns" \
    --region=northamerica-northeast1 \
    --network="projects/${GOOGLE_CLOUD_PROJECT}/global/networks/${GOOGLE_CLOUD_PROJECT}-vpc-01" \
@@ -22,6 +29,28 @@ gcloud container clusters create-auto "records-test-phac-dns" \
    --project=${GOOGLE_CLOUD_PROJECT} \
    --service-account="sa-${GOOGLE_CLOUD_PROJECT}-gke@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
 
+# create static ip address
 gcloud compute addresses create "records-test-ip" \
   --project="${GOOGLE_CLOUD_PROJECT}" \
   --region=northamerica-northeast1 \
+
+# add ASM and bind cluster to fleet
+gcloud container clusters update "records-test-phac-dns" --location=northamerica-northeast1 \
+  --fleet-project=${GOOGLE_CLOUD_PROJECT} \
+  --project=${GOOGLE_CLOUD_PROJECT}
+
+gcloud container clusters update "records-test-phac-dns" --location=northamerica-northeast1 \
+  --project=${GOOGLE_CLOUD_PROJECT} \
+  --update-labels=mesh_id=proj-$(gcloud projects describe ${GOOGLE_CLOUD_PROJECT} --format="value(projectNumber)")
+
+gcloud container fleet memberships register ${GKE_CLUSTER_NAME}-membership \
+  --gke-cluster=${GKE_LOCATION}/${GKE_CLUSTER_NAME} \
+  --enable-workload-identity \
+  --project ${PROJECT_ID}
+
+# change fleet management to automatic
+gcloud container fleet mesh update \
+ --management=automatic \
+ --memberships=records-test-phac-dns \
+ --project=${GOOGLE_CLOUD_PROJECT} \
+ --location=northamerica-northeast1
